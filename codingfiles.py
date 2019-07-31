18b7596e4ecedd9574c8403dc3ed740d4752c6cc
lkamara01 <53440506+lkamara01@users.noreply.github.com>
2019-07-31 10:10:55 -0400

    Add files via upload

---
+++ codingfiles.py
@@ -0,0 +1,143 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+
+print("Hello, my name is Lucine")
+
+5*2
+5/2
+5**2
+8/9*3
+print("5*2")
+
+def multiply(a,b):
+    multiply = a*b
+    
+    print(multiply)
+
+def sum(a,b,c):
+    sum = a+b+c
+    
+    print(sum)
+    
+sum(1,2,3)
+
+def area(a,b,c):
+    area = a*b*c
+    
+    print(area)
+
+area(1/2,3,2)
+
+def area(b,h):
+    area = (0.5)*b*h
+    
+    print("the area of the triangle is", area)
+    
+area(3,2)
+
+def area(a,b,c):
+    area = (0.5)*b+c*a
+    
+    print("the area of the trapezoid is", area)
+    
+area(2,3,4)
+
+def triArea(b,h):
+    triArea = .5*b*h
+    print("the area of the triangle with the base of", b, "and a height of", h, "is" triArea)
+    
+triArea(5,6)
+
+#cadbury chocolate examples
+def cadbury (a):
+    cadbury = a
+    print(a)
+
+cadbury(15)
+    
+def cadbury(b):
+    cadbury = b+5
+    print(b)
+    
+cadbury(15)
+
+def cadbury(a):
+    cadbury = a+5
+    print(a)
+cadbury(15)
+
+def cadburybox(a,b,c):
+    #cadburybox=("milk chocolate","dark chocolate","white chocolate")
+    print(cadburybox)
+cadburybox("white","milk","dark")
+
+def cadbury(a,b,c):
+    cadbury(a="white", b="milk", c="dark")
+    print(cadburybox)
+cadbury(a,b,c)
+
+def cadbury(a,b,c):
+    print("There is", a, ",", b, ",", c, "in the box")
+    
+cadbury("white chocolate", "dark chocolate", "milk chocolate")
+
+#greet person by name
+a = "kaya"
+print("Hello", a)
+
+name=input("please enter your name")
+
+print("your name is", name)
+
+print("enter your age")
+a = input()
+print("Your age is", a)
+
+age = input("please enter your age")
+
+print("Thank you. You entered", age)
+
+ageint = int(age)
+ageint
+
+ageint = float(age)
+
+import math
+
+dir(math)
+math.pi
+math.factorial(5)
+math.pow(6,15)
+
+math.pow(9,0.5)
+
+def cubicroot(math.pow(27,0.3)):
+          cubicroot = 3
+          print(cubicroot)
+          
+cubicroot(math.pow(27,0.333))
+
+cubicroot = math.pow(27,0.3)
+print("The cubic root of 27 is", cubicroot)
+    
+cubicroot(t)
+cubicroot = math.pow(t,1/3)
+print("the cubic root of", t, "is", cubicroot)
+
+cubicroot(27)
+
+print("the cubic root of 27")
+
+a = input("what is the cubic root of 27")
+print("The cubic root of 27 is", a)
+
+def prob(b)
+    prob= B
+    print("the cubic root of", b, "is", prob)
+
+prob(27)
